fe03be7d70a653e605d905270d147fc0e6ac070f
Ziddah Edem <ziddah.edem@gmail.com>
2020-11-04 07:21:55 +0000

    :tada: initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const menu = document.querySelector(".menu");
+const navOpen = document.querySelector(".hamburger");
+const navClose = document.querySelector(".close");
+
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.add("show");
+  }
+});
+
+navClose.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.remove("show");
+  }
+});
+
+new TypeIt("#type1", {
+  speed: 120,
+  loop: true,
+  waitUntilVisible: true,
+})
+  .type("Designer", { delay: 400 })
+  .pause(500)
+  .delete(9)
+  .type("Developer", { delay: 400 })
+  .pause(500)
+  .delete(9)
+  .go();
+
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".banner", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".hero h3", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero h4", { opacity: 0, duration: 1, delay: 3, y: -30 });
+gsap.from(".hero a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
+gsap.from(".nav-item", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.2,
+  y: 30,
+  stagger: 0.2,
+});
+gsap.from(".icons span", {
+  opacity: 0,
+  duration: 1,
+  delay: 4,
+  x: -30,
+  stagger: 0.2,
+});
